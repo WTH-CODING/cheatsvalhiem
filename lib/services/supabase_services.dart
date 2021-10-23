788726e8cc06549f84c32dca851ce9eb3ea62717
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-10-23 20:36:36 +0530

    update

--- lib/services/supabase_services.dart
+++ lib/services/supabase_services.dart
@@ -1,21 +1,16 @@
-import 'dart:convert';
-
 import 'package:cheatsvalhiem/models/spawnable_model.dart';
 import 'package:postgrest/postgrest.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseServices {
   static Future getSpawnablesFromDB() async {
-    print('FLAG1');
     PostgrestResponse response =
-        await Supabase.instance.client.from('spawnables').select().execute();
-    print('FLAG2');
-    List<Spawnable> spawnables = json
-        .decode(response.data)
-        .map<Spawnable>((spawnable) => Spawnable.fromJson(spawnable))
+        await Supabase.instance.client.from('spawnables').select().order("id").execute();
+    List<Spawnable> spawnables = response.data
+        .map<Spawnable>((spawnable) {
+          return  Spawnable.fromJson(spawnable);
+        })
         .toList();
-    print('FLAG3');
     print(spawnables.length);
-    print('FLAG4');
   }
 }
