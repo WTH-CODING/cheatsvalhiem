d3c563f26888ab49ffc3f148fb8c73541cb5ee9c
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-10-23 19:48:52 +0530

    Update landing_screen.dart

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -9,15 +9,68 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  searchCheats() {}
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (BuildContext context, int index) {
-            return CheatCard();
-          },
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: 45,
+                decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.05),
+                    borderRadius: BorderRadius.circular(12)),
+                child: TextFormField(
+                  controller: _controller,
+                  onChanged: (e) async {
+                    await searchCheats();
+                  },
+                  cursorColor: Colors.white,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: new InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    contentPadding: EdgeInsets.only(
+                        left: 15, bottom: 11, top: 11, right: 15),
+                    hintText: "Enter query",
+                    suffixIcon: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () => _controller.clear(),
+                          icon: Icon(Icons.clear),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            await searchCheats();
+                          },
+                          icon: Icon(Icons.search),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CheatCard();
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
